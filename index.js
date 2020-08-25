1fd2ef74d6e918c848878a8cd5b13d04987db319
saarhaber <saarhaber@gmail.com>
2020-08-25 16:46:14 -0400

    1 file modified

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ import "./style.css";
 
 import firebase from 'firebase';
 
-// firebase.initializeApp({
-//   databaseURL: 'https://weathersbysaar.firebaseio.com/'
-// });
+firebase.initializeApp({
+  databaseURL: 'https://weathersbysaar.firebaseio.com/'
+});
 
-// let ref = firebase.database().ref('weather');
+let ref = firebase.database().ref('weather');
 
-// let buttons = document.querySelectorAll("button");
-// buttons.forEach((button) => {
-//   button.addEventListener("click", (e) => {
-//     ref.set(e.target.textContent);
-//   });
-// });
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    ref.set(e.target.textContent);
+  });
+});
 
-// // ref.on("value",(snapshot)=> {
-// //   document.querySelector("label").textContent = snapshot.val();
-// // })
+ref.on("value",(snapshot)=> {
+  document.querySelector("label").textContent = snapshot.val();
+})
